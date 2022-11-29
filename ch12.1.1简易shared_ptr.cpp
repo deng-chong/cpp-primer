6a8fc53a62137f6d7f0e3bdee8b188cb7806256a
Deng Chong <69671764+deng-chong@users.noreply.github.com>
2022-11-29 11:07:48 +0800

    Update ch12.1.1简易shared_ptr.cpp

--- ch12.1.1简易shared_ptr.cpp
+++ ch12.1.1简易shared_ptr.cpp
@@ -1,93 +1,133 @@
-#include<vector>
-#include<string>
-#include<set>
-#include<unordered_set>
-#include<map>
-#include<unordered_map>
-#include<algorithm>
-#include<functional>
-#include<memory>
+#include<assert.h>
 #include<iostream>
-#include<sstream>
-#include <assert.h>
-using namespace std;
 
 
 template <class T>
 class SharedPtr {
+    friend std::ostream& operator<<(std::ostream& os, const SharedPtr<T>& sp) {
+        os << sp.ptr;
+        return os;
+    }
+    friend void swap(SharedPtr<T>& lhs, SharedPtr<T>& rhs) {
+        std::swap(lhs.ptr, rhs.ptr);
+        std::swap(lhs.cnt, rhs.cnt);
+    }
 public:
     SharedPtr() = default;
+
     explicit SharedPtr(T x) { // explicit: avoid implicit conversion
-        cout // << "val " << x << " "
+        std::cout // << "val " << x << " "
             << "memory successfully allocated\n";
         ptr = new T(x);
-        cnt = new size_t(1);
+        cnt = new std::size_t(1);
     }
-    SharedPtr(const SharedPtr<T>& one) {
-        cout << "construct\n";
-        if (one.ptr) {
-            ptr = one.ptr;
-            cnt = one.cnt;
-            ++*cnt;
-        }
-        cout << "constuct done\n";
+
+    SharedPtr(const SharedPtr<T>& rhs) {
+        std::cout << "copy construct\n";
+        ptr = rhs.ptr;
+        cnt = rhs.cnt;
+        if (cnt) ++*cnt;
+        std::cout << "copy constuct done\n";
     }
+
+    SharedPtr(SharedPtr<T>&& rhs) noexcept {
+        std::cout << "move construct\n";
+        ptr = rhs.ptr;
+        cnt = rhs.cnt;
+        rhs.ptr = nullptr;
+        rhs.cnt = nullptr;
+        std::cout << "move constuct done\n";
+    }
+
     SharedPtr& operator=(const SharedPtr<T>& rhs) {
-        cout << "assign\n";
+        std::cout << "copy assign\n";
         if (this != &rhs) {
-            auto tmp(rhs);
-            std::swap(ptr, tmp.ptr);
-            std::swap(cnt, tmp.cnt);
+            destroy();
+            ptr = rhs.ptr;
+            cnt = rhs.cnt;
+            if (cnt) ++*cnt;
         }
-        cout << "assign done\n";
+        std::cout << "copy assign done\n";
         return *this;
     }
-    T operator*() { return *ptr; }
-    T* operator->() { return ptr; }
-    T* get() { return ptr; }
-    ~SharedPtr() {
-        destroy();
+
+    SharedPtr& operator=(SharedPtr<T>&& rhs) {
+        std::cout << "move assign\n";
+        if (this != &rhs) {
+            destroy();
+            ptr = rhs.ptr;
+            cnt = rhs.cnt;
+            rhs.ptr = nullptr;
+            rhs.cnt = nullptr;
+        }
+        std::cout << "move assign done\n";
+        return *this;
     }
+
+    ~SharedPtr() { destroy(); }
+
+    void swap(SharedPtr<T>& rhs) {
+        std::swap(ptr, rhs.ptr);
+        std::swap(cnt, rhs.cnt);
+    }
+
+    T& operator*() const noexcept { return *ptr; }
+
+    T* operator->() const noexcept { return ptr; }
+
+    T* get() const noexcept { return ptr; }
+
+    void reset(T *p = nullptr) {
+        if (ptr != p) {
+            destroy();
+            ptr = p;
+            if (ptr) cnt = new std::size_t(1);
+        }
+    }
+    
+    explicit operator bool() const { return ptr != nullptr; } // bool type conversion
 
 private:
     T *ptr = nullptr;
-    size_t *cnt = nullptr;
+    std::size_t *cnt = nullptr;
 
     void destroy() {
         assert(ptr && cnt && *cnt || !ptr && !cnt);
-        cout << "function destroy called\n";
+        std::cout << "function destroy called\n";
         if (!ptr) return;
-        cout // << "val " << *ptr << " cnt " << *cnt << " "
-            << "reference count decreased\n";
         if (--*cnt == 0) {
-            cout << "memory successfully freed\n";
+            std::cout // << "val " << *ptr << " cnt " << *cnt << " "
+                << "reference count decreased\n";
             delete ptr;
             delete cnt;
-            ptr = nullptr;
-            cnt = nullptr;
+            std::cout << "memory successfully freed\n";
         }
+        ptr = nullptr;
+        cnt = nullptr;
     }
 };
 
 int main() {
     
     SharedPtr<int> p(1);
-    cout << "................" << endl;
+    std::cout << "................" << std::endl;
     SharedPtr<int> q;
-    cout << "................" << endl;
+    std::cout << "................" << std::endl;
     q = p;
-    cout << "................\n" << endl;
+    std::cout << "................\n" << std::endl;
     
     
     q = SharedPtr<int>(2);
-    cout << "................" << endl;
-    cout << *q << endl;
-    cout << "................" << endl; 
+    std::cout << "................" << std::endl;
+    std::cout << *q << std::endl;
+    std::cout << "................" << std::endl; 
     p = q;
-    cout << "................" << endl;
-    cout << *p << endl;
-    cout << "................" << endl;
+    std::cout << "................" << std::endl;
+    std::cout << *p << std::endl;
+    std::cout << "................" << std::endl;
     
-    cout << "\nEND............." << endl;
+    std::cout << "\nEND............." << std::endl;
+    
+    //if (p) std::cout << "**************************";
     
 }
