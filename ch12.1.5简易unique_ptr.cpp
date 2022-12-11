309767b667b540f6642f966b7796ea72c2d1b675
Deng Chong <69671764+deng-chong@users.noreply.github.com>
2022-12-11 16:17:36 +0800

    Update ch12.1.5简易unique_ptr.cpp

--- ch12.1.5简易unique_ptr.cpp
+++ ch12.1.5简易unique_ptr.cpp
@@ -7,10 +7,15 @@ using namespace std;
 template <typename T, typename D = std::default_delete<T>>
 class UniquePtr {
    public:
-    UniquePtr(T* p = nullptr, D d = D()) : ptr(p), del(d) {}
-    UniquePtr(const T& one) : ptr(new T(one)) {}
-    UniquePtr(const UniquePtr&) = delete;                                                                          // copy constructor not permitted
-    UniquePtr& operator=(const UniquePtr&) = delete;                                                               // copy assignment not permitted
+    template <typename U = T>
+    UniquePtr(U* p = nullptr, D d = D()) : ptr(p), del(d) {}
+
+    template <typename U = T>
+    UniquePtr(const U& one) : ptr(new U(one)) {}
+
+    UniquePtr(const UniquePtr&) = delete;             // copy constructor not permitted
+    UniquePtr& operator=(const UniquePtr&) = delete;  // copy assignment not permitted
+
     UniquePtr(UniquePtr&& one) noexcept : ptr(std::move(one.ptr)), del(std::move(one.del)) { one.ptr = nullptr; }  // move constructor
     UniquePtr& operator=(UniquePtr&& one) noexcept {
         if (this != &one) {
@@ -34,7 +39,8 @@ class UniquePtr {
         std::swap(del, one.del);
     }
 
-    void reset(T* p = nullptr) {
+    template <typename U = T>
+    void reset(U* p = nullptr) {
         if (p == ptr) return;
         del(ptr);
         ptr = p;
@@ -66,9 +72,9 @@ class UniquePtr {
 template <typename T, typename D>
 void swap(UniquePtr<T, D>& lhs, UniquePtr<T, D>& rhs) { lhs.swap(rhs); }
 
+template <typename T>
 class Deleter {
    public:
-    template <typename T>
     void operator()(T* p) const {
         std::cout << "Deleting UniquePtr" << std::endl;
         delete p;
@@ -81,12 +87,37 @@ UniquePtr<T, D> f() {
     return UniquePtr<T, D>();
 }
 
-template class UniquePtr<vector<string>>;
+class A {
+   public:
+    int a = 0;
+    virtual ~A() = default;
+    virtual void show() const {
+        std::cout << "A: " << a << "\n";
+    }
+};
+
+class B : public A {
+   public:
+    std::string s = "B";
+    virtual void show() const override {
+        std::cout << "B: " << a << " " << s << " "
+                  << "\n";
+    }
+};
 
 int main() {
+    A* pa = new A();
+    B* pb = new B();
+    UniquePtr<A> spa(pa), spb(pb);
+    spa->show();
+    spb->show();
+    spa.reset(spb.release());
+    spa->show();
+    //spb->show();
+
     vector<string> v1{"A", "B", "C"}, v2{"Book", "See", "Hi"};
-    UniquePtr<vector<string>, Deleter> p1(v1), p2(v2);
-    UniquePtr<vector<string>, Deleter> p(f<vector<string>, Deleter>());
+    UniquePtr<vector<string>, Deleter<vector<string>>> p1(v1), p2(v2);
+    UniquePtr<vector<string>, Deleter<vector<string>>> p(f<vector<string>, Deleter<vector<string>>>());
     p.reset(p1.release());
     p.show();
     p1.reset(p2.release());
