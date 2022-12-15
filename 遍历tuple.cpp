4d67d61872a43236583e600df74ef85835ec3104
Deng Chong <69671764+deng-chong@users.noreply.github.com>
2022-12-15 14:18:38 +0800

    Create 遍历tuple.cpp

---
+++ 遍历tuple.cpp
@@ -0,0 +1,69 @@
+//方法1
+
+template <typename Tuple, std::size_t N>
+struct TuplePrinter {
+    static std::ostream& print(std::ostream& os, const Tuple& t) {
+        return TuplePrinter<Tuple, N - 1>::print(os, t) << ", " << std::get<N - 1>(t);
+    }
+};
+
+template <typename Tuple>
+struct TuplePrinter<Tuple, 1> {
+    static std::ostream& print(std::ostream& os, const Tuple& t) {
+        return os << std::get<0>(t);
+    }
+};
+
+template <typename... Args>
+std::ostream& operator<<(std::ostream& os, const std::tuple<Args...>& t) {
+    os << "(";
+    constexpr std::size_t N = sizeof...(Args);
+    if (N) TuplePrinter<decltype(t), N>::print(os, t);
+    os << ")";
+    return os;
+}
+
+// 方法2
+
+template <std::size_t I, typename... Args>
+typename std::enable_if<(I + 1 == sizeof...(Args)), std::ostream&>::type
+_tuple_print(std::ostream& os, std::tuple<Args...>& t) {
+    return os << std::get<I>(t);
+}
+
+template <std::size_t I, typename... Args>
+typename std::enable_if<(I + 1 < sizeof...(Args)), std::ostream&>::type
+_tuple_print(std::ostream& os, std::tuple<Args...>& t) {
+    os << std::get<I>(t) << ", ";
+    return _tuple_print<I + 1, Args...>(os, t);
+}
+
+template <typename... Args>
+std::ostream& operator<<(std::ostream& os, std::tuple<Args...>& t) {
+    os << "(";
+    if (sizeof...(Args)) _tuple_print<0, Args...>(os, t);
+    os << ")";
+    return os;
+}
+
+// 方法3
+
+template <std::size_t I, typename... Args>
+std::ostream& _tuple_print(std::ostream&, std::tuple<Tp...>& t) {
+    if (I < sizeof...(Args))
+        os << ", " << std::get<I>(t);
+    if constexpr (I + 1 != sizeof...(Args)) // if constexpr: since c++17
+        _tuple_print<I + 1, Args...>(os, t);
+    return os;
+}
+
+template <typename... Args>
+std::ostream& operator<<(std::ostream& os, std::tuple<Args...>& t) {
+    os << "(";
+    if (sizeof...(Args)) {
+        os << std::get<0>(t);
+        _tuple_print<1, Args...>(os, t);
+    }
+    os << ")";
+    return os;
+}
