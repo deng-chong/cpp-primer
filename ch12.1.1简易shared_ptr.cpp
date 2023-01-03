25b8359f7ab46cb6a2466712b4c274c8d95563a6
Deng Chong <69671764+deng-chong@users.noreply.github.com>
2023-01-03 10:57:06 +0800

    Update ch12.1.1简易shared_ptr.cpp

--- ch12.1.1简易shared_ptr.cpp
+++ ch12.1.1简易shared_ptr.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+#include <memory>
+using namespace std;
+
 #include <cassert>
 #include <functional>
 #include <iostream>
@@ -6,10 +10,12 @@
 
 template <typename T>
 class SharedPtr {
+    template <typename U>
+    friend class SharedPtr;
+
    public:
-    // default deleter: nullptr (function pointer)
     template <typename U = T>
-    SharedPtr(U *p = nullptr, std::function<void(T *)> d = nullptr) : ptr(p), cnt(nullptr), del(d) {
+    SharedPtr(U *p = nullptr) : ptr(p), cnt(nullptr) {
         if (ptr)
             cnt = new std::size_t(1);
     }
@@ -21,31 +27,50 @@ class SharedPtr {
         cnt = new std::size_t(1);
     }
 
-    SharedPtr(const SharedPtr &rhs) {
-        std::cout << "copy construct\n";
+    SharedPtr(const SharedPtr &rhs) noexcept {
+        std::cout << "copy construct T\n";
         ptr = rhs.ptr;
         cnt = rhs.cnt;
-        del = rhs.del;
         assert(!ptr && !cnt || ptr && cnt);
         if (cnt)
             ++*cnt;
-        std::cout << "copy construct done\n";
+        std::cout << "copy construct T done\n";
+    }
+
+    template <typename U>
+    SharedPtr(const SharedPtr<U> &rhs) noexcept {
+        std::cout << "copy construct U\n";
+        ptr = rhs.ptr;
+        cnt = rhs.cnt;
+        assert(!ptr && !cnt || ptr && cnt);
+        if (cnt)
+            ++*cnt;
+        std::cout << "copy construct U done\n";
     }
 
     SharedPtr(SharedPtr &&rhs) noexcept {
-        std::cout << "move construct\n";
+        std::cout << "move construct T\n";
         ptr = rhs.ptr;
         cnt = rhs.cnt;
-        del = rhs.del;
         rhs.ptr = nullptr;
         rhs.cnt = nullptr;
-        rhs.del = nullptr;
         assert(!ptr && !cnt || ptr && cnt);
-        std::cout << "move construct done\n";
+        std::cout << "move construct T done\n";
     }
 
-    SharedPtr &operator=(const SharedPtr &rhs) {
-        std::cout << "copy assign\n";
+    template <typename U>
+    SharedPtr(SharedPtr<U> &&rhs) noexcept {
+        std::cout << "move construct U\n";
+        ptr = rhs.ptr;
+        cnt = rhs.cnt;
+        rhs.ptr = nullptr;
+        rhs.cnt = nullptr;
+        assert(!ptr && !cnt || ptr && cnt);
+        std::cout << "move construct U done\n";
+    }
+
+    SharedPtr &operator=(const SharedPtr &rhs) noexcept {
+        std::cout << "copy assign T\n";
         if (this != &rhs) {
             if (ptr != rhs.ptr) {
                 destroy();
@@ -54,25 +79,53 @@ class SharedPtr {
             }
             ptr = rhs.ptr;
             cnt = rhs.cnt;
-            del = rhs.del;
         }
-        std::cout << "copy assign done\n";
+        std::cout << "copy assign T done\n";
         return *this;
     }
 
-    SharedPtr &operator=(SharedPtr &&rhs) {
-        std::cout << "move assign\n";
+    template <typename U>
+    SharedPtr &operator=(const SharedPtr<U> &rhs) noexcept {
+        std::cout << "copy assign U\n";
+        if (this != &rhs) {
+            if (ptr != rhs.ptr) {
+                destroy();
+                if (rhs.cnt)
+                    ++*rhs.cnt;
+            }
+            ptr = rhs.ptr;
+            cnt = rhs.cnt;
+        }
+        std::cout << "copy assign U done\n";
+        return *this;
+    }
+
+    SharedPtr &operator=(SharedPtr &&rhs) noexcept {
+        std::cout << "move assign T\n";
         if (this != &rhs) {
             if (ptr != rhs.ptr)
                 destroy();
             ptr = rhs.ptr;
             cnt = rhs.cnt;
-            del = rhs.del;
             rhs.ptr = nullptr;
             rhs.cnt = nullptr;
-            rhs.del = nullptr;
         }
-        std::cout << "move assign done\n";
+        std::cout << "move assign T done\n";
+        return *this;
+    }
+
+    template <typename U>
+    SharedPtr &operator=(SharedPtr<U> &&rhs) noexcept {
+        std::cout << "move assign U\n";
+        if (this != &rhs) {
+            if (ptr != rhs.ptr)
+                destroy();
+            ptr = rhs.ptr;
+            cnt = rhs.cnt;
+            rhs.ptr = nullptr;
+            rhs.cnt = nullptr;
+        }
+        std::cout << "move assign U done\n";
         return *this;
     }
 
@@ -81,7 +134,6 @@ class SharedPtr {
     void swap(SharedPtr &rhs) {
         std::swap(ptr, rhs.ptr);
         std::swap(cnt, rhs.cnt);
-        std::swap(del, rhs.del);
     }
 
     std::size_t use_count() const noexcept { return cnt ? *cnt : 0; }
@@ -97,14 +149,13 @@ class SharedPtr {
     T *get() const noexcept { return ptr; }
 
     template <typename U = T>
-    void reset(U *p = nullptr, std::function<void(T *)> d = nullptr) {
+    void reset(U *p = nullptr) {
         if (ptr != p) {
             destroy();
             ptr = p;
             if (ptr)
                 cnt = new std::size_t(1);
         }
-        del = d;
     }
 
     explicit operator bool() const { return ptr != nullptr; }  // bool type conversion
@@ -112,20 +163,18 @@ class SharedPtr {
    private:
     T *ptr;
     std::size_t *cnt;
-    std::function<void(T *)> del;
 
     void destroy() {
         std::cout << "function destroy called\n";
         assert(!ptr && !cnt || ptr && cnt && *cnt);
         if (cnt && --*cnt == 0) {
             std::cout << "reference count decreased\n";
-            del ? del(ptr) : delete ptr;
+            delete ptr;
             delete cnt;
             std::cout << "memory successfully freed\n";
         }
         ptr = nullptr;
         cnt = nullptr;
-        del = nullptr;
     }
 };
 
@@ -157,10 +206,19 @@ class B : public A {
     }
 };
 
+void fun(const SharedPtr<A> &) {  // 形参也可以是 SharedPtr<A>，但不能是 SharedPtr<A>&
+    std::cout << "Get in\n";
+}
+
 int main() {
+    auto pointee = makeShared<B>();
+    std::cout << "Before\n";
+    fun(pointee);
+    std::cout << "End\n";
+
     auto ptr = makeShared<std::string>(10, 'a');
     std::cout << ptr.use_count() << " " << *ptr << std::endl;
-   
+
     A *pa = new A();
     B *pb = new B();
     SharedPtr<A> spa(pa), spb(pb);
